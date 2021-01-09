2fe1ce86d503ce414ca6fc2ed5c373a96c56b59b
Mikhail9030 <gulaevmihail112@gmail.com>
2021-01-09 20:56:08 +0300

    Commit flexible 3

--- src/test/java/ru/netology/domain/constructor/RadioTest.java
+++ src/test/java/ru/netology/domain/constructor/RadioTest.java
@@ -1,5 +1,6 @@
 package ru.netology.domain.constructor;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -7,17 +8,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
+    @Test
+    void setMaxStationTest(){
+        int expected=14;
+        Radio radio=new Radio(expected);
+
+        assertEquals(expected, radio.getMaxStation());
+    }
+
     @ParameterizedTest
     @CsvSource(
             value = {
-                    "'From zero station'; 0; 1",
-                    "'Go last station'; 18; 19",
-                    "'More maximum station'; 20; 0"
+                    "'From first station'; 5; 1; 0",
+                    "'Last station'; 14; 14; 14",
+                    "'More maximum station'; 14; 17; 0",
+                    "'Less minimum station'; 5; -1; 0"
             }
             , delimiter = ';'
     )
-    void pressNextStationTest(String name, int start, int expected) {
-        Radio radio=new Radio();
+    void setStationNumberTest(String name, int max, int start, int expected) {
+        Radio radio=new Radio(max);
+        radio.setCurrentStation(start);
+
+        assertEquals(expected, radio.getCurrentStation());
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "'From first station'; 5; 0; 1",
+                    "'Go last station'; 5; 4; 5",
+                    "'More maximum station'; 5; 5; 0"
+            }
+            , delimiter = ';'
+    )
+    void pressNextStationTest(String name, int max, int start, int expected) {
+        Radio radio=new Radio(max);
         radio.setCurrentStation(start);
 
         radio.pressNextStation();
@@ -27,14 +53,14 @@ class RadioTest {
     @ParameterizedTest
     @CsvSource(
             value = {
-                    "'From middle station'; 15; 14",
-                    "'Go first station'; 1; 0",
-                    "'Less minimum station'; 0; 19"
+                    "'From last station'; 15; 15; 14",
+                    "'Go first station'; 5; 1; 0",
+                    "'Less minimum station'; 10; 0; 5"
             }
             , delimiter = ';'
     )
-    void pressPrevStationTest(String name, int start, int expected) {
-        Radio radio=new Radio();
+    void pressPrevStationTest(String name, int max, int start, int expected) {
+        Radio radio=new Radio(max);
         radio.setCurrentStation(start);
 
         radio.pressPrevStation();
@@ -61,7 +87,7 @@ class RadioTest {
     @ParameterizedTest
     @CsvSource(
             value = {
-                    "'Middle volume'; 50; 40",
+                    "'Max volume'; 100; 99",
                     "'Go min volume'; 1; 0",
                     "'Less minimum volume'; 0; 0"
             }
